aa61b004a70c757714fd562c19e2062e4473e8d9
cosmore <cosmore@cc.com>
2023-02-21 22:46:19 +0800

    exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple:i32)->i32 {
+    match apple {
+        35=>70,
+        40=>80,
+        41=>41,
+        65=>65,
+        _ => todo!(),
+    }
+
+}
 
 // Don't modify this function!
 #[test]
